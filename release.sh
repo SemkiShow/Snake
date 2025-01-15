0ef942d80ba5ce2d6c010178c8d2c0a99f96cf2d
SemkiShow <mgdeveloper123@gmail.com>
2025-01-15 21:33:14 +0100

    Added release.sh

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Compiling for Linux
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+cp build/bin/main main
+
+# Compiling for Windows
+# cd build_win
+# cmake .. --toolchain ../mingw-w64-x86_64.cmake
+# make
+# cd ..
+# cp build_win/bin/main.exe main.exe
+
+# Zipping the build
+rm release.zip
+# zip release.zip maps/* main main.exe JetBrainsMonoNerdFont-Medium.ttf LICENSE README.md
+# rm main main.exe
+zip release.zip maps/* main JetBrainsMonoNerdFont-Medium.ttf LICENSE README.md
+rm main
+
+# Creating a GitHub release
+gh release create
