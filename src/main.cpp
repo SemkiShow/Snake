48d66ce02ae3da2e188bab2ac4fe34cdf415f0d1
SemkiShow <mgdeveloper123@gmail.com>
2025-01-15 21:26:56 +0100

    Added apple spawining

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <random>
+#include <ctime>
 
-unsigned int cellSize = 50;
+unsigned int horizontalCellsNumber = 32;
 unsigned int windowWidth = 16*50*2;
 unsigned int windowHeight = 9*50*2;
 unsigned int windowSize[2] = {windowWidth, windowHeight};
-unsigned int horizontalCellsNumber = windowWidth / cellSize;
+unsigned int cellSize = windowWidth / horizontalCellsNumber;
 bool verticalSync = true;
 
 bool isGameOver = false;
@@ -15,12 +17,19 @@ bool isGameOver = false;
 class Snake
 {
     public:
-        unsigned int position[2];
         std::vector<int> body;
         char direction = 'R';
         sf::Color color = sf::Color(100, 250, 50);
         std::vector<char> keyBuffer;
-        double speed = 10;
+        double speed = 2;
+};
+
+class Apple
+{
+    public:
+        unsigned int position;
+        sf::Color color = sf::Color(255, 0, 0);
+        unsigned int nutritionValue = 1;
 };
 
 int main()
@@ -32,10 +41,14 @@ int main()
     snake.body.push_back(2);
     snake.body.push_back(3);
     snake.body.push_back(4);
+    srand(time(0));
 
     // SFML init
     sf::RenderWindow window(sf::VideoMode({windowSize[0], windowSize[1]}), "Snake");
     sf::Font font("./JetBrainsMonoNerdFont-Medium.ttf");
+    std::vector<Apple> apples;
+    apples.push_back(Apple());
+    apples[0].position = rand() % (horizontalCellsNumber * windowHeight / cellSize);
     if (verticalSync)
     {
         window.setFramerateLimit(144);
@@ -85,7 +98,7 @@ int main()
         }
 
         // Snake physics stuff
-        if (delayClock.getElapsedTime().asSeconds() >= 1 / (snake.speed * snake.body.size()) && !isGameOver)
+        if (delayClock.getElapsedTime().asSeconds() >= 1 / (snake.speed * sqrt(snake.body.size())) && !isGameOver)
         {
             delayClock.restart();
 
@@ -105,12 +118,25 @@ int main()
             // Remove the last tail piece
             snake.body.erase(snake.body.begin());
             // Move the head
-            snake.body.push_back(snake.body[snake.body.size() - 1] + snakeDirectionInteger);
+            snake.body.push_back(snake.body[snake.body.size()-1] + snakeDirectionInteger);
+
+            // Check apple collision
+            for (int i = 0; i < apples.size(); i++)
+            {
+                for (int j = 0; j < snake.body.size(); j++)
+                {
+                    if (snake.body[j] == apples[i].position)
+                    {
+                        apples[0].position = rand() % (horizontalCellsNumber * windowHeight / cellSize);
+                        snake.body.push_back(snake.body[snake.body.size()-1] + snakeDirectionInteger);
+                    }
+                }
+            }
 
             // Check collision with the walls
             if (snake.body[snake.body.size()-1]+horizontalCellsNumber < horizontalCellsNumber && snake.direction == 'U')
                 snake.body[snake.body.size()-1] += horizontalCellsNumber * windowHeight / cellSize;
-            if (snake.body[snake.body.size()-1]-horizontalCellsNumber >= (horizontalCellsNumber-1) * windowHeight / cellSize && snake.direction == 'D')
+            if (snake.body[snake.body.size()-1] > (horizontalCellsNumber-1) * windowHeight / cellSize && snake.direction == 'D')
                 snake.body[snake.body.size()-1] -= horizontalCellsNumber * windowHeight / cellSize;
             if ((snake.body[snake.body.size()-1]+1) % horizontalCellsNumber == 0 && snake.direction == 'L')
                 snake.body[snake.body.size()-1] += horizontalCellsNumber;
@@ -142,11 +168,21 @@ int main()
             window.draw(bodyPiece);
         }
 
+        // Drawing the apples
+        for (int i = 0; i < apples.size(); i++)
+        {
+            sf::RectangleShape apple({cellSize * 1.f, cellSize * 1.f});
+            apple.setFillColor(apples[i].color);
+            apple.setPosition({(apples[i].position % horizontalCellsNumber) * cellSize * 1.f, 
+                (apples[i].position / horizontalCellsNumber) * cellSize * 1.f});
+            window.draw(apple);
+        }
+
         // Print game over is necessary
         if (isGameOver)
         {
             sf::Text gameOver(font);
-            gameOver.setCharacterSize(250);
+            gameOver.setCharacterSize(0.135f * sqrt(windowWidth * windowWidth + windowHeight * windowHeight));
             gameOver.setPosition({windowWidth * .05f, windowHeight * .3f});
             gameOver.setFillColor(sf::Color(255, 0, 0));
             gameOver.setString("Game Over!");
