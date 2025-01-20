95269df0b7198c30a3ff7075f1d8da454650f848
SemkiShow <mgdeveloper123@gmail.com>
2025-01-20 15:11:38 +0100

    Added Windows support

--- release.sh
+++ release.sh
@@ -5,18 +5,19 @@ cmake --build build --config Release
 cp build/bin/main main
 
 # Compiling for Windows
-# cd build_win
-# cmake .. --toolchain ../mingw-w64-x86_64.cmake
-# make
-# cd ..
+cd build_win
+cmake .. --toolchain ~/mingw-w64-x86_64.cmake
+make
+cd ..
 # cp build_win/bin/main.exe main.exe
+cp build_win/bin/* .
 
 # Zipping the build
 rm release.zip
-# zip release.zip main main.exe assets/* LICENSE README.md
-# rm main main.exe
-zip release.zip main assets/* LICENSE README.md
-rm main
+zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll assets/* LICENSE README.md
+rm main main.exe libgcc_s_seh-1.dll libstdc++-6.dll
+# zip release.zip main assets/* LICENSE README.md
+# rm main
 
 # Creating a GitHub release
 gh release create
