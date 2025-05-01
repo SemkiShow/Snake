54f56409f806da02f8a631c23109fbe1a07e0b9d
SemkiShow <mgdeveloper123@gmail.com>
2025-05-01 23:24:40 +0200

    Remove unnecessary dlls

--- release.sh
+++ release.sh
@@ -25,7 +25,7 @@ cp build_win/bin/main.exe .
 # Zipping the build
 rm release.zip
 ./reset_save_files.sh
-zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll assets/* LICENSE README.md score.txt settings.txt
+zip release.zip main main.exe assets/* LICENSE README.md score.txt settings.txt
 rm main main.exe
 
 # Creating a GitHub release
