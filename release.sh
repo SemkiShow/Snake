37f7284e3027288b5d9ed75cba7dbcf3af360f7c
SemkiShow <mgdeveloper123@gmail.com>
2025-05-01 19:55:01 +0200

    Make the release build static

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ if [ ! -d build ]; then
     mkdir build
 fi
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-static"
 make
 cd ..
 cp build/bin/main main
@@ -17,7 +17,7 @@ if [ ! -d build_win ]; then
     mkdir build_win
 fi
 cd build_win
-cmake .. -DCMAKE_BUILD_TYPE=Release --toolchain ../mingw-w64-x86_64.cmake
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-static" --toolchain ../mingw-w64-x86_64.cmake
 make
 cd ..
 cp build_win/bin/main.exe .
@@ -25,7 +25,7 @@ cp build_win/bin/main.exe .
 # Zipping the build
 rm release.zip
 ./reset_save_files.sh
-zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll assets/* LICENSE README.md score.txt settings.txt
+zip release.zip main main.exe assets/* LICENSE README.md score.txt settings.txt
 rm main main.exe
 
 # Creating a GitHub release
