eb1fd93967f88c4cf25f61bb99d00ceec4b659d7
SemkiShow <mgdeveloper123@gmail.com>
2025-05-22 21:22:59 +0200

    Remove unnecessary files from .gitignore and add release.zip removal at the end of release.sh

--- release.sh
+++ release.sh
@@ -23,10 +23,10 @@ cd ..
 cp build_win/bin/main.exe .
 
 # Zipping the build
-rm release.zip
 ./reset_save_files.sh
 zip release.zip main main.exe assets/* LICENSE README.md score.txt settings.txt
 rm main main.exe
 
 # Creating a GitHub release
 gh release create $1 release.zip
+rm release.zip
